f9c53d6ae79251567e6677fd23e993e5c28aa706
Diogo Felipe <67284614+diogof651@users.noreply.github.com>
2023-05-16 09:19:50 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.yaml"); // substitua pelo caminho do arquivo Swagger
