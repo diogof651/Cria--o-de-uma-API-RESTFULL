cf99957a57b9380fd9593db6748455c022be7278
Darubo <g_alves@ufms.br>
2023-05-19 10:39:08 -0400

    alteracoes

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
+const swaggerUi = require('swagger-ui-express');
 const express = require('express');
 const app = express();
 const fs = require('fs');
- // Definir rota para o arquivo YAML do Swagger
-app.get('/swagger', (req, res) => {
-  try {
-    // Ler arquivo YAML
-    const yamlFile = fs.readFileSync('./swagger.yaml', 'utf8');
-    // Definir resposta HTTP com o conteúdo do arquivo YAML do Swagger
-    res.set('Content-Type', 'application/x-yaml; charset=utf-8');
-    res.send(yamlFile);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Erro interno do servidor');
-  }
-});
- // Inicializar servidor
+const yaml = require('js-yaml');
+
+// Ler arquivo YAML
+const yamlFile = fs.readFileSync('./swagger.yaml', 'utf8');
+// Converter YAML para JSON
+const swaggerDocument = yaml.load(yamlFile);
+
+// Rota para exibir a interface do Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Inicializar servidor
 const porta = process.env.PORT || 3000;
 app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}...`));
